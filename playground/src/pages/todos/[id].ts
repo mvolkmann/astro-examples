2bfc8994717b007cdcb275b6f7a350f42ec32920
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-12-21 15:13:55 -0600

    progress

--- playground/src/pages/todos/[id].ts
+++ playground/src/pages/todos/[id].ts
@@ -20,11 +20,11 @@ if (!globalThis.todoMap) {
   globalThis.todoMap = new Map<number, Todo>();
 }
 
+// See https://davetayls.me/blog/2023-05-21-convert-a-readablestream-to-a-string-in-javascript.
 async function getBody(request: Request): Promise<string> {
   const { body } = request;
   let text = "";
   if (body) {
-    // See https://davetayls.me/blog/2023-05-21-convert-a-readablestream-to-a-string-in-javascript.
     const reader = body.getReader();
     const decoder = new TextDecoder();
     while (true) {
@@ -37,22 +37,29 @@ async function getBody(request: Request): Promise<string> {
 }
 
 export async function PUT({ params, request }: APIContext) {
+  const { id } = params;
+  if (!id) return new Response('missing "id" parameter', { status: 400 });
+  const idNumber = Number(id);
   const text = await getBody(request);
   const todo = JSON.parse(text) as Todo;
-  const exists = globalThis.todoMap.has(todo.id);
-  if (exists) globalThis.todoMap.set(todo.id, todo);
+  todo.id = idNumber; // ensures the id matches the path parameter
+  const exists = globalThis.todoMap.has(idNumber);
+  if (exists) globalThis.todoMap.set(idNumber, todo);
   const status = exists ? 200 : 404;
   return new Response(JSON.stringify(todo), { status });
 }
 
 export async function PATCH({ params, request }: APIContext) {
+  const { id } = params;
+  if (!id) return new Response('missing "id" parameter', { status: 400 });
+  const idNumber = Number(id);
   const text = await getBody(request);
   const updates = JSON.parse(text) as Todo;
-  const { id } = updates;
-  let todo = globalThis.todoMap.get(id);
+  updates.id = idNumber; // ensures the id matches the path parameter
+  let todo = globalThis.todoMap.get(idNumber);
   if (todo) {
     todo = { ...todo, ...updates };
-    globalThis.todoMap.set(id, todo);
+    globalThis.todoMap.set(idNumber, todo);
   }
   const status = todo ? 200 : 404;
   return new Response(JSON.stringify(todo), { status });
@@ -62,8 +69,6 @@ export async function DELETE({ params, request }: APIContext) {
   const { id } = params;
   if (!id) return new Response('missing "id" parameter', { status: 400 });
   const idNumber = Number(id);
-  console.log("todos/[id].ts : idNumber =", idNumber);
-  console.log("todos/[id].ts : globalThis.todoMap =", globalThis.todoMap);
   const status = globalThis.todoMap.delete(idNumber) ? 200 : 404;
   return new Response("", { status });
 }
