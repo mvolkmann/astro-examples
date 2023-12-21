dae1e6050711b1e259cd8def7c4255eed26a591b
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-12-21 16:25:24 -0600

    progress

--- playground/src/pages/todos/[id].ts
+++ playground/src/pages/todos/[id].ts
@@ -36,9 +36,16 @@ async function getBody(request: Request): Promise<string> {
   return text;
 }
 
+export async function GET({ params }: APIContext) {
+  const { id } = params;
+  const idNumber = Number(id);
+  const todo = globalThis.todoMap.get(idNumber);
+  return new Response(JSON.stringify(todo), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
 export async function PUT({ params, request }: APIContext) {
   const { id } = params;
-  if (!id) return new Response('missing "id" parameter', { status: 400 });
   const idNumber = Number(id);
   const text = await getBody(request);
   const todo = JSON.parse(text) as Todo;
@@ -51,7 +58,6 @@ export async function PUT({ params, request }: APIContext) {
 
 export async function PATCH({ params, request }: APIContext) {
   const { id } = params;
-  if (!id) return new Response('missing "id" parameter', { status: 400 });
   const idNumber = Number(id);
   const text = await getBody(request);
   const updates = JSON.parse(text) as Todo;
@@ -68,6 +74,7 @@ export async function PATCH({ params, request }: APIContext) {
 export async function DELETE({ params, request }: APIContext) {
   const { id } = params;
   if (!id) return new Response('missing "id" parameter', { status: 400 });
+
   const idNumber = Number(id);
   const status = globalThis.todoMap.delete(idNumber) ? 200 : 404;
   return new Response("", { status });
