abdadbf7028aa3a18bc4b7009209772b8687bdb7
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-12-23 17:08:08 -0600

    progress

--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import node from "@astrojs/node";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), svelte()],
   output: "server",
   adapter: node({
     mode: "standalone"
---
+++ playground/src/components/Counter.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let label = '';
+
+  let count = 0;
+</script>
+
+<div class="row">
+  {#if label}
+    <div>{label}</div>
+  {/if}
+  <button disabled={count <= 0} on:click={() => count--}>-</button>
+  <h1>{count}</h1>
+  <button on:click={() => count++}>+</button>
+</div>
+
+<style>
+  button, h1 {
+    margin: 0;
+  }
+
+  .row {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+</style>
