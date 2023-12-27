bd306bb82e54d35f058362ee18be9bfca7a25478
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-12-27 14:35:55 -0600

    progress

--- nanostores-demo/src/stores.ts
+++ nanostores-demo/src/stores.ts
@@ -1,10 +1,10 @@
 // This is using the vanilla JS version of nanostores.
-import { atom, computed, map } from "nanostores";
+import {atom, computed, map} from 'nanostores';
 
 export const $score = atom<number>(0);
 
 export const $status = computed($score, (score: number) =>
-  score === 10 ? "win" : "play"
+  score === 10 ? 'win' : 'play'
 );
 
 type Dog = {
@@ -13,16 +13,16 @@ type Dog = {
 };
 
 export const $dog = map<Dog>({
-  name: "Comet",
-  breed: "Whippet",
+  name: 'Comet',
+  breed: 'Whippet'
 });
 
 export function toggleDog() {
   // The get method retrieves the entire object.
   // There is no getKey method.
-  const { name } = $dog.get();
+  const {name} = $dog.get();
   // The set method changes the entire object.
   // The setKey method changes a single property.
-  $dog.setKey("name", name === "Comet" ? "Oscar" : "Comet");
-  $dog.setKey("breed", name === "Comet" ? "GSP" : "Whippet");
+  $dog.setKey('name', name === 'Comet' ? 'Oscar' : 'Comet');
+  $dog.setKey('breed', name === 'Comet' ? 'GSP' : 'Whippet');
 }
