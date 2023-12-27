ca392771093388adfb8f11b83aec8c2819c061f6
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-12-27 14:21:16 -0600

    progress

--- nanostores-demo/src/stores.ts
+++ nanostores-demo/src/stores.ts
@@ -1,8 +1,19 @@
-import { atom, map } from "nanostores";
+import { atom, computed, map } from "nanostores";
+import { getParsedCommandLineOfConfigFile } from "typescript";
 
 export const $score = atom<number>(0);
 
-export const $dog = map({
+export const $status = computed($score, (score: number) => {
+  console.log("stores.ts computed: score =", score, typeof score);
+  return score === 10 ? "win" : "play";
+});
+
+type Dog = {
+  name: string;
+  breed: string;
+};
+
+export const $dog = map<Dog>({
   name: "Comet",
   breed: "Whippet",
 });
