077ff5e59e04a39aed25308b970c2245a778eea2
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-12-27 13:53:29 -0600

    progress

--- nanostores-demo/src/stores.ts
+++ nanostores-demo/src/stores.ts
@@ -2,7 +2,7 @@ import { atom, map } from "nanostores";
 
 export const $score = atom<number>(0);
 
-export const dog = map({
-  name: "Dog",
+export const $dog = map({
+  name: "Comet",
   breed: "Whippet",
 });
