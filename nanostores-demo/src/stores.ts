9a83f2bd4fc299370f639d58b3641f908107b258
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-12-27 14:33:40 -0600

    progress

--- nanostores-demo/src/stores.ts
+++ nanostores-demo/src/stores.ts
@@ -16,3 +16,13 @@ export const $dog = map<Dog>({
   name: "Comet",
   breed: "Whippet",
 });
+
+export function toggleDog() {
+  // The get method retrieves the entire object.
+  // There is no getKey method.
+  const { name } = $dog.get();
+  // The set method changes the entire object.
+  // The setKey method changes a single property.
+  $dog.setKey("name", name === "Comet" ? "Oscar" : "Comet");
+  $dog.setKey("breed", name === "Comet" ? "GSP" : "Whippet");
+}
