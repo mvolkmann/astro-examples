cc338b1caf2d934d2b1ae42be14a5763ca7e9710
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-12-28 16:39:27 -0600

    progress

--- content-collections/astro.config.mjs
+++ content-collections/astro.config.mjs
@@ -5,6 +5,5 @@ import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
-  output: 'server'
+  integrations: [tailwind(), alpinejs()]
 });
