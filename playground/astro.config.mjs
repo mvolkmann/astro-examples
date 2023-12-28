01d1d14812591a4c731b2e2aa069f26d84bae25f
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-12-28 13:37:06 -0600

    progress

--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -1,18 +1,18 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import node from "@astrojs/node";
-import svelte from "@astrojs/svelte";
-import alpinejs from "@astrojs/alpinejs";
-import icon from "astro-icon";
+import {defineConfig} from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import node from '@astrojs/node';
+import svelte from '@astrojs/svelte';
+import alpinejs from '@astrojs/alpinejs';
+import icon from 'astro-icon';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), svelte(), alpinejs(), icon(), react()],
-  output: "server",
+  output: 'server',
   adapter: node({
-    mode: "standalone"
+    mode: 'standalone'
   })
 });
