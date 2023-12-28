af24920063730962bf8902bf1d268096343257f4
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-12-27 18:29:27 -0600

    This branch shows that we don't even need nanostores if only using Astro components.

--- nanostores-demo/src/stores.ts
+++ nanostores-demo/src/stores.ts
@@ -1,44 +1,26 @@
-// This is using the vanilla JS version of nanostores.
-import {atom, computed, map} from 'nanostores';
-
+// We can use only Alpine to share data between Astro components.
+// But we need nanostores to share data with other kinds of components
+// like React, Svelte, and Vue.
 type Dog = {
   name: string;
   breed: string;
 };
 
-type ScoreData = {score: number, status: string};
-
-export const $score = atom<number>(0);
-
-export const $status = computed($score, (score: number) =>
-  score === 10 ? 'win' : 'play'
-);
-
-const $dog = map<Dog>({
-  name: 'Comet',
-  breed: 'Whippet'
-});
+type XData = {dog: Dog, score: number, status: string};
 
 // @ts-ignore
 globalThis.ns = {
-  subscribeToDog(data: Dog) {
-    $dog.subscribe(dog => {
-      data.name = dog.name;
-      data.breed = dog.breed;
-    });
+  setData(xData: XData) {
+    this.xData = xData;
   },
-  subscribeToScore(data: ScoreData) {
-    $score.subscribe(score => data.score = score);
-    $status.subscribe(status => data.status = status);
+  setScore(score: number) {
+    this.xData.score = score;
+    this.xData.status = score === 10 ? 'win' : 'play'
   },
   toggleDog() {
-    // The get method retrieves the entire object.
-    // There is no getKey method.
-    const {name} = $dog.get();
-
-    // The set method changes the entire object.
-    // The setKey method changes a single property.
-    $dog.setKey('name', name === 'Comet' ? 'Oscar' : 'Comet');
-    $dog.setKey('breed', name === 'Comet' ? 'GSP' : 'Whippet');
+    const { name } = this.xData.dog;
+    const isComet = name === 'Comet';
+    this.xData.dog.name = isComet ? 'Oscar' : 'Comet';
+    this.xData.dog.breed = isComet ? 'GSP' : 'Whippet';
   }
 };
