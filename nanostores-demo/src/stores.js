125e04fdc293ceb048b460be588587f8c68c6f05
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-12-31 15:11:48 -0600

    progress

--- nanostores-demo/src/stores.js
+++ nanostores-demo/src/stores.js
@@ -1,10 +1,17 @@
 import {atom, computed, map} from 'nanostores';
+import {persistentAtom} from '@nanostores/persistent';
+
+const count = persistentAtom('count', 0, {
+  encode: JSON.stringify,
+  decode: JSON.parse
+});
 
 const score = atom(0);
 
 // @ts-ignore
 globalThis.stores = {
-  count: atom(1),
+  // count: atom(1), // This does not use persistent storage.
+  count, // This uses persistent storage.
   dog: map({
     name: 'Comet',
     breed: 'Whippet'
