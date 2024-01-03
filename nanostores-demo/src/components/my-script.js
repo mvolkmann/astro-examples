919aac82c644777261ba10d95694b952d5f86df5
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-01-03 15:08:22 -0600

    progress

--- nanostores-demo/src/components/my-script.js
+++ nanostores-demo/src/components/my-script.js
@@ -1,3 +1,4 @@
+console.log('my-script.js: entered');
 globalThis.ns = {
   fn4() {
     console.log('in fn4');
--- nanostores-demo/src/my-module.js
+++ nanostores-demo/src/my-module.js
@@ -1,3 +1,4 @@
+console.log('my-module.js: entered');
 export function fn3() {
   console.log('in fn3');
 }
